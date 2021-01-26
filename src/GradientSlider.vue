ea065d291e127cda49f852a31c93acb2ef82e6d8
Geon George <geongeorge98@gmail.com>
2021-01-26 20:42:02 +0530

    Fixes css

--- src/GradientSlider.vue
+++ src/GradientSlider.vue
@@ -145,17 +145,6 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-$fullGradient: linear-gradient(
-  to right,
-  hsl(0, 100%, 50%),
-  hsl(60, 100%, 50%),
-  hsl(120, 100%, 50%),
-  hsl(180, 100%, 50%),
-  hsl(240, 100%, 50%),
-  hsl(300, 100%, 50%),
-  hsl(360, 100%, 50%)
-);
-
 .gradient-slider {
   width: 100%;
   height: 20px;
